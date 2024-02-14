0256681c2d752d9c019612829e0b650bd62c9bb1
Rayan <nasrallr@mcmaster.ca>
2024-02-14 17:58:05 -0500

    Fixed package issues

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team219/Explorer.java
@@ -0,0 +1,64 @@
+package ca.mcmaster.se2aa4.island.team219;
+
+import java.io.StringReader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import eu.ace_design.island.bot.IExplorerRaid;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+
+public class Explorer implements IExplorerRaid {
+
+    boolean fly = true;
+    int num = 0;
+
+    private final Logger logger = LogManager.getLogger();
+
+    @Override
+    public void initialize(String s) {
+        logger.info("** Initializing the Exploration Command Center");
+        JSONObject info = new JSONObject(new JSONTokener(new StringReader(s)));
+        logger.info("** Initialization info:\n {}",info.toString(2));
+        String direction = info.getString("heading");
+        Integer batteryLevel = info.getInt("budget");
+        logger.info("The drone is facing {}", direction);
+        logger.info("Battery level is {}", batteryLevel);
+    }
+
+    @Override
+    public String takeDecision() {
+        JSONObject decision = new JSONObject();
+        if (fly && num<30) {
+            decision.put("action", "scan");
+            num++;
+            fly = false;
+        } else if (num<30) {
+            decision.put("action", "fly");
+            num++;
+            fly = true;
+        } else {
+            decision.put("action", "stop");
+        }
+        return decision.toString();
+    }
+
+    @Override
+    public void acknowledgeResults(String s) {
+        JSONObject response = new JSONObject(new JSONTokener(new StringReader(s)));
+        logger.info("** Response received:\n"+response.toString(2));
+        Integer cost = response.getInt("cost");
+        logger.info("The cost of the action was {}", cost);
+        String status = response.getString("status");
+        logger.info("The status of the drone is {}", status);
+        JSONObject extraInfo = response.getJSONObject("extras");
+        logger.info("Additional information received: {}", extraInfo);
+    }
+
+    @Override
+    public String deliverFinalReport() {
+        return "no creek found";
+    }
+
+}
