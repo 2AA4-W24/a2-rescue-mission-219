344c12a826ee7a8ed3da3f36114f25e1a9be355e
Rayan <nasrallr@mcmaster.ca>
2024-02-19 19:07:23 -0500

    Created batteryLevel class (close #3)

---
+++ src/main/java/ca/batteryLevel.java
@@ -0,0 +1,26 @@
+package ca;
+
+public class batteryLevel {
+
+    private int battery;
+
+    public batteryLevel(int battery){
+        this.battery = battery;
+    }
+
+    public int getBatteryLevel(){
+        return battery;
+    }
+
+    public void decreaseBattery(int cost){
+        battery = battery - cost;
+
+    }
+
+    public boolean batteryLevelLow(int battery){
+        if (battery == 1){
+            return true;
+        }
+        return false;
+    }
+}
