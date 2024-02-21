06d83e18529a5c1f5a6b0c7c76063637955f3f13
nasrallr <nasrallr@mcmaster.ca>
2024-02-21 02:57:08 -0500

    Fix Turn class (close #19)

--- src/main/java/ca/mcmaster/se2aa4/island/team219/Turn.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team219/Turn.java
@@ -2,25 +2,36 @@ package ca.mcmaster.se2aa4.island.team219;
 
 public enum Turn {
 
-    north("west","east"), 
-    south("east","west"), 
-    east("north","south"), 
-    west("south","north");
-
-    private final Turn left;
-    private final Turn right;
-
-    Turn(String left, String right) {
-        this.left = Turn.valueOf(left);
-        this.right = Turn.valueOf(right);
-    }
+    N, S, E, W;
 
     public Turn left() {
-        return this.left;
+        switch(this) {
+            case N: return Turn.W;
+            case S: return Turn.E;
+            case E: return Turn.N;
+            case W: return Turn.S;
+            default: return this;
+        }
     }
 
     public Turn right() {
-        return this.right;
+        switch(this) {
+            case N: return Turn.E;
+            case S: return Turn.W;
+            case E: return Turn.S;
+            case W: return Turn.N;
+            default: return this;
+        }
     }
 
+    @Override
+    public String toString() {
+        switch(this) {
+            case N: return "N";
+            case S: return "S";
+            case E: return "E";
+            case W: return "W";
+            default: return "Unknown";
+        }
+    }
 }
