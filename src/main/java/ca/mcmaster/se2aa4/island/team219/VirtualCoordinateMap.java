6a839966f3450a430326b9883946cb9c18e313d6
moustafa <moustm4@mcmaster.ca>
2024-02-29 19:44:05 -0500

    added the dependancy for JUnit testing and successfully tested VirtualCoordinateMap (close #29) added default statements for VirtualCoordinateMap (close #30)

--- src/main/java/ca/mcmaster/se2aa4/island/team219/VirtualCoordinateMap.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team219/VirtualCoordinateMap.java
@@ -24,45 +24,57 @@ public class VirtualCoordinateMap {
             case S:
                 y -= 1;
                 break;
+            default:
+                break;
         }
     }
 
     public void turnLeft() {
-        if (currentDirection == Turn.N) {
-            x -= 1; 
-            y += 1; 
-        } else if (currentDirection == Turn.S) {
-            x += 1; 
-            y -= 1; 
-        } else if (currentDirection == Turn.E) {
-            x += 1; 
-            y += 1; 
-        } else if (currentDirection == Turn.W) {
-            x -= 1; 
-            y -= 1; 
+        switch (currentDirection) {
+            case N:
+                x -= 1; 
+                y += 1;
+                break;
+            case S:
+                x += 1; 
+                y -= 1;
+                break;
+            case E:
+                x += 1; 
+                y += 1;
+                break;
+            case W:
+                x -= 1; 
+                y -= 1;
+                break;
+            default:
+                break;
         }
-
         currentDirection = currentDirection.left();
-
     }
 
     public void turnRight() {
-        if (currentDirection == Turn.N) {
-            x += 1; 
-            y += 1;
-        } else if (currentDirection == Turn.S) {
-            x -= 1; 
-            y -= 1; 
-        } else if (currentDirection == Turn.E) {
-            x += 1; 
-            y -= 1; 
-        } else if (currentDirection == Turn.W) {
-            x -= 1; 
-            y += 1; 
+        switch (currentDirection) {
+            case N:
+                x += 1; 
+                y += 1;
+                break;
+            case S:
+                x -= 1; 
+                y -= 1;
+                break;
+            case E:
+                x += 1; 
+                y -= 1;
+                break;
+            case W:
+                x -= 1; 
+                y += 1;
+                break;
+            default:
+                break;
         }
-
         currentDirection = currentDirection.right();
-
     }
 
     public String getCurrentPosition() {
