3420132f599fda2ca0fc8a4f6bdb56bb716b1b4b
Muhaymenxzcx <aibrahim1359@gmail.com>
2024-03-09 23:01:41 -0500

    added new test cases to test folder

--- src/test/java/ca/mcmaster/se2aa4/island/team219/VirtualCoordinateMapTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team219/VirtualCoordinateMapTest.java
@@ -1,90 +1,93 @@
 package ca.mcmaster.se2aa4.island.team219;
 
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class VirtualCoordinateMapTest {
 
+    VirtualCoordinateMap map;
+
     @Test
     void testMoveForwardFacingEast() {
-        VirtualCoordinateMap map = new VirtualCoordinateMap(Turn.E);
+        map = new VirtualCoordinateMap(Turn.E);
         map.moveForward();
         assertEquals("(1,0)", map.getCurrentPosition());
     }
 
     @Test
     void testMoveForwardFacingWest() {
-        VirtualCoordinateMap map = new VirtualCoordinateMap(Turn.W);
+        map = new VirtualCoordinateMap(Turn.W);
         map.moveForward();
         assertEquals("(-1,0)", map.getCurrentPosition());
     }
 
     @Test
     void testMoveForwardFacingNorth() {
-        VirtualCoordinateMap map = new VirtualCoordinateMap(Turn.N);
+        map = new VirtualCoordinateMap(Turn.N);
         map.moveForward();
         assertEquals("(0,1)", map.getCurrentPosition());
     }
 
     @Test
     void testMoveForwardFacingSouth() {
-        VirtualCoordinateMap map = new VirtualCoordinateMap(Turn.S);
+        map = new VirtualCoordinateMap(Turn.S);
         map.moveForward();
         assertEquals("(0,-1)", map.getCurrentPosition());
     }
 
     @Test
     void testTurnRightFromEast() {
-        VirtualCoordinateMap map = new VirtualCoordinateMap(Turn.E);
+        map = new VirtualCoordinateMap(Turn.E);
         map.turnRight();
         assertEquals("(1,-1)", map.getCurrentPosition());
     }
 
     @Test
     void testTurnLeftFromEast() {
-        VirtualCoordinateMap map = new VirtualCoordinateMap(Turn.E);
+        map = new VirtualCoordinateMap(Turn.E);
         map.turnLeft();
         assertEquals("(1,1)", map.getCurrentPosition());
     }
 
     @Test
     void testTurnRightFromWest() {
-        VirtualCoordinateMap map = new VirtualCoordinateMap(Turn.W);
+        map = new VirtualCoordinateMap(Turn.W);
         map.turnRight();
         assertEquals("(-1,1)", map.getCurrentPosition());
     }
 
     @Test
     void testTurnLeftFromWest() {
-        VirtualCoordinateMap map = new VirtualCoordinateMap(Turn.W);
+        map = new VirtualCoordinateMap(Turn.W);
         map.turnLeft();
         assertEquals("(-1,-1)", map.getCurrentPosition());
     }
 
     @Test
     void testTurnRightFromNorth() {
-        VirtualCoordinateMap map = new VirtualCoordinateMap(Turn.N);
+        map = new VirtualCoordinateMap(Turn.N);
         map.turnRight();
         assertEquals("(1,1)", map.getCurrentPosition());
     }
 
     @Test
     void testTurnLeftFromNorth() {
-        VirtualCoordinateMap map = new VirtualCoordinateMap(Turn.N);
+        map = new VirtualCoordinateMap(Turn.N);
         map.turnLeft();
         assertEquals("(-1,1)", map.getCurrentPosition());
     }
 
     @Test
     void testTurnRightFromSouth() {
-        VirtualCoordinateMap map = new VirtualCoordinateMap(Turn.S);
+        map = new VirtualCoordinateMap(Turn.S);
         map.turnRight();
         assertEquals("(-1,-1)", map.getCurrentPosition());
     }
 
     @Test
     void testTurnLeftFromSouth() {
-        VirtualCoordinateMap map = new VirtualCoordinateMap(Turn.S);
+        map = new VirtualCoordinateMap(Turn.S);
         map.turnLeft();
     
         assertEquals("(1,-1)", map.getCurrentPosition());
@@ -92,10 +95,10 @@ public class VirtualCoordinateMapTest {
 
     @Test
     void testMultipleMovements() {
-        VirtualCoordinateMap map = new VirtualCoordinateMap(Turn.N);
-        map.moveForward(); // Move north
-        map.turnRight(); // Face east
-        map.moveForward(); // Move east
+        map = new VirtualCoordinateMap(Turn.N);
+        map.moveForward();
+        map.turnRight(); 
+        map.moveForward(); 
         assertEquals("(2,2)", map.getCurrentPosition());
     }
 }
