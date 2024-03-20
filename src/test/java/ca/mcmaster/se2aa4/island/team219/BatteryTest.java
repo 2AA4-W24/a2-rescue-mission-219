db466816faa786b8c04797725353ea83a44d6b1c
moustafa <moustm4@mcmaster.ca>
2024-03-20 01:36:13 -0400

    adding meaningful and helpful messages for current JUnit tests in BatteryTest class close (#54)

--- src/test/java/ca/mcmaster/se2aa4/island/team219/BatteryTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team219/BatteryTest.java
@@ -3,33 +3,31 @@ package ca.mcmaster.se2aa4.island.team219;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class BatteryLevelTest {
+public class BatteryTest {
 
     @Test
     void testGetBatteryLevel() {
         Battery battery = new Battery(100);
-        battery.getBatteryLevel();
-        assertEquals(100, battery.getBatteryLevel());
+        int actualBatteryLevel = battery.getBatteryLevel();
+        assertEquals(100, actualBatteryLevel, "Battery level should be initialized to 100.");
     }
 
     @Test
     void testDecreaseBattery() {
         Battery battery = new Battery(100);
         battery.decreaseBattery(20);
-        assertEquals(80, battery.getBatteryLevel());
+        assertEquals(80, battery.getBatteryLevel(), "Decreasing the battery by 20 should result in a battery level of 80.");
     }
 
     @Test
     void testBatteryLevelLowFalse() {
         Battery battery = new Battery(50);
-        assertFalse(battery.batteryLevelLow());
+        assertFalse(battery.batteryLevelLow(), "Battery level at 50 should not be considered low.");
     }
 
     @Test
     void testBatteryLevelLowTrue() {
         Battery battery = new Battery(30);
-        assertTrue(battery.batteryLevelLow());
+        assertTrue(battery.batteryLevelLow(), "Battery level at or below 30 should be considered low.");
     }
-
 }
-
